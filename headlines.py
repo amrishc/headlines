b3bbc33e7d4996c3430ac8368597f5c7f481e68e
Amrish Chopra <achopra@vmware.com>
2017-10-31 16:28:46 -0700

    Added a new Form for Input

--- headlines.py
+++ headlines.py
@@ -1,18 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import feedparser
-
 
 
 app = Flask(__name__)
 
 RSS_FEEDS = {'google': 'https://news.google.com/rss?gl=US&ned=us&hl=en',
-             'cnn': 'http://rss.cnn.com/rss/cnn_topstories.rss',
-             'bbc': 'http://feeds.bbci.co.uk/news/rss.xml'}
+             'cnn': 'http://rss.cnn.com/rss/edition.rss',
+             'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
+             'iol': 'http://www.iol.co.za/cmlink/1.640'}
 
 
 @app.route('/')
-@app.route('/<publication>')
-def get_news(publication='bbc'):
+def get_news():
+    query = request.args.get("publication")
+    if not query or query.lower() not in RSS_FEEDS:
+        publication = "bbc"
+    else:
+        publication = query.lower()
+
     feed = feedparser.parse(RSS_FEEDS[publication])
     return render_template("home.html",articles=feed['entries'])
 
